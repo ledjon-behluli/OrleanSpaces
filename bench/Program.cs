7dc4e9d72787cdb5f7e0eedf0885186b524ce058
Ledjon Behluli <Ledjon@notiphy.io>
2022-09-11 21:05:52 +0200

    .

--- bench/Program.cs
+++ bench/Program.cs
@@ -1,8 +1,13 @@
 ﻿using BenchmarkDotNet.Running;
+using OrleanSpaces.Primitives;
 
 //BenchmarkRunner.Run<SpaceUnitBenchmarks>();
 //BenchmarkRunner.Run<SpaceTupleBenchmarks>();
 //BenchmarkRunner.Run<SpaceTemplateBenchmarks>();
 BenchmarkRunner.Run<TestRun>();
+
+//SpaceTupleAlloc.Create(1);
+//SpaceTupleAlloc.Create("a");
+//SpaceTupleAlloc.Create((1, "a", 1.5f, 'b', 1.2m, 2, 0x00, -3, 2.4d));
 
 Console.ReadKey();
