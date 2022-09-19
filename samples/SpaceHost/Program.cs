bb48ae66f0f4aaf9357ab6a63e5da1fa3d3ed6c0
Ledjon Behluli <Ledjon@notiphy.io>
2022-09-19 18:44:16 +0200

    all good

--- samples/SpaceHost/Program.cs
+++ samples/SpaceHost/Program.cs
@@ -8,7 +8,31 @@ var host = new SiloHostBuilder()
     .UseLocalhostClustering()
     .AddSimpleMessageStreamProvider(Constants.PubSubProvider)
     .AddMemoryGrainStorage(Constants.PubSubStore)
+
+    // Choose what storage mechanism you want to use for storing the tuple space state.
+
     .AddMemoryGrainStorage(Constants.TupleSpaceStore)
+
+    //.AddAzureTableGrainStorage(Constants.TupleSpaceStore, options =>
+    //{
+    //    options.UseJson = true;
+    //    options.ConfigureTableServiceClient("UseDevelopmentStorage=true");
+    //})
+
+    //.AddAzureBlobGrainStorage(Constants.TupleSpaceStore, options =>
+    //{
+    //    options.UseJson = true;
+    //    options.ConfigureBlobServiceClient("UseDevelopmentStorage=true");
+    //})
+
+    // More infos on ADO.NET: https://dotnet.github.io/orleans/docs/host/configuration_guide/adonet_configuration.html
+    //.AddAdoNetGrainStorage(Constants.TupleSpaceStore, options =>
+    //{
+    //    options.UseJsonFormat = true;
+    //    options.Invariant = "Npgsql";
+    //    options.ConnectionString = "Host=localhost;Database=OrleanSpaces;Username=postgres;Password=postgres";
+    //})
+
     .AddTupleSpace()
     .ConfigureLogging(builder => builder.AddConsole())
     .Build();
