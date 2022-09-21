1aa107fb19797e5235bfac66a4c7cf2e8a1c7ad3
Ledjon Behluli <Ledjon@notiphy.io>
2022-09-21 18:35:11 +0200

    added gh workflows

--- src/OrleanSpaces/SpaceAgentProvider.cs
+++ src/OrleanSpaces/SpaceAgentProvider.cs
@@ -5,7 +5,7 @@ public interface ISpaceAgentProvider
     /// <summary>
     /// Returns an <see cref="ISpaceAgent"/> that is used to interact with the tuple space.
     /// </summary>
-    /// <remarks><i>Method is idempotant.<br/>Method is thread-safe.</i></remarks>
+    /// <remarks><i>Method is thread-safe.</i></remarks>
     ValueTask<ISpaceAgent> GetAsync();
 }
 
