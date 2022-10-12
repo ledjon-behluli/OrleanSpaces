71d51da9ef5503175590f003932df53403922e50
Ledjon Behluli <Ledjon@notiphy.io>
2022-10-12 22:32:28 +0200

    wip

--- samples/AnalyzersOffenders/Program.cs
+++ samples/AnalyzersOffenders/Program.cs
@@ -1,3 +1,15 @@
 ﻿using OrleanSpaces.Tuples;
 
-SpaceTuple _ = new SpaceTuple();
+#region OSA001
+
+SpaceUnit unit1 = new SpaceUnit();
+SpaceUnit unit2 = new();
+SpaceUnit unit3 = default(SpaceUnit);
+SpaceUnit unit4 = default;
+
+SpaceTuple tuple1 = new SpaceTuple();
+SpaceTuple tuple2 = new();
+SpaceTuple tuple3 = default(SpaceTuple);
+SpaceTuple tuple4 = default;
+
+#endregion
