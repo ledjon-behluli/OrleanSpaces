a9cee01a5c1043726c29a30ca1b72592db49e7a4
Ledjon Behluli <Ledjon@notiphy.io>
2022-11-04 23:46:13 +0100

    OSA004 wip

---
+++ src/OrleanSpaces.Analyzers/OSA004/InvalidMethodCallOnCurrentConfigAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+using System.Collections.Immutable;
+
+namespace OrleanSpaces.Analyzers.OSA004;
+
+/// <summary>
+/// Warns when a method which is supported only on full configuration, is called when partial configuration is used.
+/// </summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+internal sealed class InvalidMethodCallOnCurrentConfigAnalyzer : DiagnosticAnalyzer
+{
+    public static readonly DiagnosticDescriptor Diagnostic = new(
+        id: "OSA004",
+        category: Categories.Usage,
+        defaultSeverity: DiagnosticSeverity.Warning,
+        isEnabledByDefault: true,
+        title: "The invocation will result in a runtime exception due to current configuration.",
+        messageFormat: "The invocation of '{0}' will result in a runtime exception due to current configuration.",
+        helpLinkUri: "https://github.com/ledjon-behluli/OrleanSpaces/blob/master/docs/OrleanSpaces.Analyzers/Rules/OSA004.md");
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Diagnostic);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
