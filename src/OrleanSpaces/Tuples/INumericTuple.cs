731e05737cde3ab3ecc343e2bcba33c833a2ef00
Ledjon Behluli <Ledjon@notiphy.io>
2023-04-15 23:59:37 +0200

    .

--- src/OrleanSpaces/Tuples/INumericTuple.cs
+++ src/OrleanSpaces/Tuples/INumericTuple.cs
@@ -8,10 +8,3 @@ internal interface INumericTuple<T, H> : ISpaceTuple<T, H>
 {
     Span<T> Data { get; }
 }
-
-internal interface IStronglyTypedTuple<T, H> : ISpaceTuple<T, H>
-    where T : struct, INumber<T>
-    where H : ISpaceTuple<T, H>
-{
-    Span<T> Data { get; }
-}
