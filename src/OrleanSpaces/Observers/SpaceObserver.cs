1e05855ed661da25e788ced7a5a6f7150852efbe
Ledjon Behluli <Ledjon@notiphy.io>
2023-07-11 17:39:27 +0200

    .

--- src/OrleanSpaces/Observers/SpaceObserver.cs
+++ src/OrleanSpaces/Observers/SpaceObserver.cs
@@ -31,7 +31,7 @@ public abstract class SpaceObserver<T> : ISpaceObserver<T>
             return;
         }
 
-        if (action.Type == TupleActionType.Clear && type.HasFlag(Contractions))
+        if (action.Type == TupleActionType.Clear && type.HasFlag(Flattenings))
         {
             await OnFlatteningAsync(cancellationToken);
             return;
