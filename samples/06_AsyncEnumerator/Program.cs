22ac2ebf72131029e3bfa569b7f5378abe0b8433
Ledjon Behluli <Ledjon@notiphy.io>
2023-07-13 22:17:41 +0200

    .

---
+++ samples/06_AsyncEnumerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using OrleanSpaces;
+using OrleanSpaces.Tuples;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+var host = new HostBuilder()
+    .ConfigureAppConfiguration(config => config.AddJsonFile("appsettings.json"))
+    .UseOrleansClient(builder =>
+    {
+        builder.AddOrleanSpaces();
+        builder.UseLocalhostClustering();
+        builder.AddMemoryStreams(Constants.PubSubProvider);
+    })
+    .ConfigureLogging((context, builder) =>
+    {
+        builder.ClearProviders();
+        builder.AddConfiguration(context.Configuration.GetSection("Logging"));
+        builder.AddConsole();
+    })
+    .Build();
+
+await host.StartAsync();
+
+Console.WriteLine("Connected to the tuple space.\n\n");
+
+ISpaceAgent agent = host.Services.GetRequiredService<ISpaceAgent>();
+
+// TODO: Continue
+
+Console.WriteLine("\nPress any key to terminate...");
+Console.ReadKey();
+
+await host.StopAsync();
---
+++ samples/07_ClientServer/Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using OrleanSpaces;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .UseOrleans(siloBuilder =>
+    {
+        siloBuilder.UseLocalhostClustering();
+        siloBuilder.AddMemoryStreams(Constants.PubSubProvider);
+        siloBuilder.AddMemoryGrainStorage(Constants.PubSubStore);
+        siloBuilder.AddMemoryGrainStorage(Constants.StorageName);
+    })
+    .ConfigureLogging(builder => builder.AddConsole())
+    .Build();
+
+await host.StartAsync();
+
+Console.WriteLine("\nPress any key to terminate...\n");
+Console.ReadLine();
+
+await host.StopAsync();
