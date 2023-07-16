dca0e05624878c0f6bc0b4aab3b5b8420026ac6a
Ledjon Behluli <Ledjon@notiphy.io>
2023-07-16 14:09:10 +0200

    .

--- samples/Sandbox/Program.cs
+++ samples/Sandbox/Program.cs
@@ -1,9 +1,6 @@
 ﻿using OrleanSpaces;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using OrleanSpaces.Tuples;
-using OrleanSpaces.Tuples.Specialized;
-using System.Runtime.CompilerServices;
 
 var host = new HostBuilder()
     .UseOrleansClient(builder =>
@@ -21,33 +18,7 @@ Console.WriteLine("Connected to the tuple space.\n\n");
 
 // test anything here...
 
-OrleanSpaces.Tuples.ISpaceTuple tuple = new SpaceTuple();
-ISpaceTemplate template = new SpaceTemplate(1);
-
-ISpaceTuple<int> tuple1 = new IntTuple();
-ISpaceTemplate<int> template1 = new IntTemplate(1);
-
 Console.WriteLine("\nPress any key to terminate...\n");
 Console.ReadKey();
 
 await host.StopAsync();
-
-class TestTuple : ISpaceTuple
-{
-    public int Length => 0;
-}
-
-class TestTe : ISpaceTemplate
-{
-    public int Length => 0;
-}
-
-class TestInt : ISpaceTemplate<int>
-{
-    private int? value;
-    public ref readonly int? this[int index] => ref value;
-    public int Length => 0;
-    public ReadOnlySpan<int?>.Enumerator GetEnumerator() => new();
-}
-
-class A { public void B(ISpaceTuple tuple) { } }
